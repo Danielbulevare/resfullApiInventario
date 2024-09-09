893876378012faf362ce089d47a8cf50166c2c91
Daniel Rod <jkdaniel06@gmail.com>
2024-09-09 09:49:24 -0600

    Primer commit

---
+++ resfullApiInventario/src/main/java/com/prueba/resfullApiInventario/ResfullApiInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.resfullApiInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfullApiInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResfullApiInventarioApplication.class, args);
+	}
+
+}
---
+++ resfullApiInventario/src/test/java/com/prueba/resfullApiInventario/ResfullApiInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.resfullApiInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfullApiInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
