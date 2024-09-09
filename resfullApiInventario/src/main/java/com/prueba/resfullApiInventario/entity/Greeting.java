cd863e282af7c857f7d804afa200b1564e22f5dd
Daniel Rod <jkdaniel06@gmail.com>
2024-09-09 11:49:39 -0600

    Creación de apiRest Greeting

---
+++ resfullApiInventario/src/main/java/com/prueba/resfullApiInventario/entity/Greeting.java
@@ -0,0 +1,13 @@
+package com.prueba.resfullApiInventario.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+    private Long id;
+    private String content;
+}
